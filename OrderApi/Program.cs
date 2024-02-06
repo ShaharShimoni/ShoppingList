156321ed0f20c6771ac00f55395c64a645e0c20a
Shahar_Glikman <shaharglikman00@gmail.com>
2024-02-06 12:02:49 +0200

    Fix BE

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -1,13 +1,6 @@
 using System.Text.Json;
 namespace OrderApi
 {
-    public class OrderRequest
-    {
-        public required string Name { get; set; }
-        public required string Address { get; set; }
-        public required string Email { get; set; }
-        public JsonElement Products { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
